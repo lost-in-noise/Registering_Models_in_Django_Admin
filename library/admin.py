191d7d2d80f153a9f9ffff7622c045ea285b7ee9
lost-in-noise <nancykikvadze@gmail.com>
2024-10-12 22:02:15 +0400

    feat: Register and customize Author and Book models in the admin interface

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birth_date')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('birth_date',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'isbn')
+    search_fields = ('title', 'isbn', 'author__first_name', 'author__last_name')
+    list_filter = ('published_date', 'author')
